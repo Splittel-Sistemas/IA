f11a5847fddd9079efa224d65a6e8c0d7355321c
aaron0609ac <58867893+aaron0609ac@users.noreply.github.com>
2024-04-04 12:13:15 -0600

    Inicial

--- config.py
+++
@@ -1,9 +0,0 @@
-# Configuración de la conexión a HANA
-HANA_CONFIG = {
-    'address': '192.168.2.19',
-    'port': 30015,
-    'user': 'USR_LECTURA',
-    'password': 'SPL.Lectura202xx3.',
-    'database': 'HN_FIBREMEX'
-}
-
--- passenger_wsgi.py
+++
@@ -1,1 +0,0 @@
-from main import Api as application
