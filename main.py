9ead6b7ac47fb6626633f3c012166ce8dc7ed39f
aaron0609ac <58867893+aaron0609ac@users.noreply.github.com>
2024-04-04 18:02:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ def recibir_mensajes():
 
 
 if __name__ == '__main__':
-    #- Debug
-    app.run(debug=True, port=5000)
+    # Debug
+    app.run(debug=False, port=5000)
      # Production
     #http_server = WSGIServer(('', 443), app)
     #http_server.serve_forever()
